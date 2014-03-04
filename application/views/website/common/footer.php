dbf06ff171a5dcdeb5806ecadb7f9b5998c6648b
her0satr <her0satr@yahoo.com>
2014-03-03 19:49:37 -0800

    update

--- application/views/website/common/footer.php
+++
@@ -1,3 +0,0 @@
-<div id="cnt-footer">
-	&copy; 2013. All Rights Reserved.
-</div>
--- application/views/website/common/header.php
+++
@@ -1,4 +0,0 @@
-<div id="cnt-header">
-	<a href="<?php echo base_url(); ?>">HOME</a> |
-	<a href="<?php echo base_url('login'); ?>">LOGIN</a>
-</div>
